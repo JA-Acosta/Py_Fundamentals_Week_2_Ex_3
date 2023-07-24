2ace360f8727c89eab0eed84d5e6a98f468498d0
JAAR <joe.aaron.acosta@outlook.com>
2023-07-24 17:21:52 -0400

    Version 0

---
+++ main.py
@@ -0,0 +1,10 @@
+'''
+>>> JAAR
+>>> 07/24/2023
+>>> Practicing Fundamentals Program 8
+>>> Version 0
+'''
+
+'''
+>>> Generates a program that will ask the user for a number, will then determine if the number is even or odd. Afterwards, will calculate the radius of a circle using the number and finally will calculate the factorial for the number inputted.
+'''
